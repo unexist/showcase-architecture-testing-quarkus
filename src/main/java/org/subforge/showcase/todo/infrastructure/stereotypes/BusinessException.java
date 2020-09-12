8ad358bbd08384bc24caf33edfec936857503054
Christoph Kappel <unexist@subforge.org>
2020-09-12 13:28:18 +0200

    Initial import

---
+++ src/main/java/org/subforge/showcase/todo/infrastructure/stereotypes/BusinessException.java
@@ -0,0 +1,15 @@
+package org.subforge.showcase.todo.infrastructure.stereotypes;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+/**
+ * Represents a business exception. Business exceptions signal attempts to invalidly change business invariants.
+ */
+@Target(TYPE)
+@Documented
+public @interface BusinessException {
+
+}
