682a3cdb76c510d5025c2fd60095e56e98650e4a
Christoph Kappel <unexist@subforge.org>
2020-09-24 16:06:30 +0200

    Added ArchUnit example

---
+++ src/test/java/org/subforge/showcase/todo/domain/model/TodoArchUnitTest.java
@@ -0,0 +1,54 @@
+/**
+ * @package Quarkus-jqAssistant-Showcase
+ *
+ * @file Contrived tests with ArchUnit
+ * @copyright 2019 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
+
+package org.subforge.showcase.todo.domain.model;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import net.jqwik.api.Property;
+import org.slf4j.Logger;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
+
+@AnalyzeClasses(packages = "org.subforge.showcase")
+public class TodoArchUnitTest {
+
+    @ArchTest
+    static final ArchRule testShouldBePrefixedWithTest =
+            methods()
+                .that()
+                    .areAnnotatedWith(Property.class)
+                .should()
+                    .haveNameMatching("^test.*")
+                .because("Test classes must be prefixed with 'Test'");
+
+    @ArchTest
+    static final ArchRule loggersShouldBeStaticAndPrivate =
+            fields()
+                .that()
+                    .haveRawType(Logger.class)
+                .should()
+                    .bePrivate()
+                    .andShould().beStatic()
+                    .andShould().beFinal()
+                .because("Loggers have to be that way");
+
+    @ArchTest
+    static final ArchRule checkPlacement =
+            classes()
+                .that()
+                    .resideInAnyPackage("..test..")
+                .should()
+                    .bePublic();
+}
